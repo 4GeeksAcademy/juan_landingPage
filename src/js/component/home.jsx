3edba2a2c0f5d90c2b60d93673807f686040686a
juanruizl <160032371+juanruizl@users.noreply.github.com>
2024-09-27 10:49:08 +0000

    home

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,28 +10,36 @@ const Home = () => {
             <div className="container mt-5">
                 <Jumbotron /> {/* renderizo el Jumbotron creado en la pagina aparte */}
                 <div className="row mt-5">
-                    <div className="col-md-4">
+                    <div className="col-md-3">
 						{/* renderizo el Card creado en la pagina aparte pero aunque no lo haga en las siguientes columnas lo hago dentro de cada columna */}
                         <Card 
-                            imagen=""
+                            imagen="https://i0.wp.com/www.silocreativo.com/wp-content/uploads/2018/01/imagen-aleatoria-destacada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
                             titulo="Carta 1"
                             descripcion="descripcion para la primera carta."
                             link="#"
                         />
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-md-3">
                         <Card 
-                            imagen=""
+                            imagen="https://i0.wp.com/www.silocreativo.com/wp-content/uploads/2018/01/imagen-aleatoria-destacada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
                             titulo="Carta 2"
                             descripcion="descripcion para la segunda carta."
                             link="#"
                         />
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-md-3">
                         <Card 
-                            imagen=""
+                            imagen="https://i0.wp.com/www.silocreativo.com/wp-content/uploads/2018/01/imagen-aleatoria-destacada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
                             titulo="Carta 3"
                             descripcion="descripcion para la tercera carta."
+                            link="#"
+                        />
+                    </div>
+					<div className="col-md-3">
+                        <Card 
+                            imagen="https://i0.wp.com/www.silocreativo.com/wp-content/uploads/2018/01/imagen-aleatoria-destacada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
+                            titulo="Carta 4"
+                            descripcion="descripcion para la cuarta carta."
                             link="#"
                         />
                     </div>
